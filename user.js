529e58e426e913c2e9dfab4ba2e048e24a9d903e
d13g0 <diego.cantor@gmail.com>
2022-09-30 15:54:21 -0400

    solving symlink issues by reversing the direction of the symlink

--- user.js
+++ user.js
@@ -1,1 +1,20 @@
-../../Library/Thunderbird/Profiles/2y15eqlu.default-release/user.js
+// Custom user preferences
+
+// mailnews tags
+user_pref("mailnews.tags.$label1.color", "#be5046");
+user_pref("mailnews.tags.$label1.tag", "Important");
+
+user_pref("mailnews.tags.$label2.color", "#e5c07b");
+user_pref("mailnews.tags.$label2.tag", "Work");
+
+user_pref("mailnews.tags.$label3.color", "#98C379");
+user_pref("mailnews.tags.$label3.tag", "Personal");
+
+user_pref("mailnews.tags.$label4.color", "#61AFEF");
+user_pref("mailnews.tags.$label4.tag", "To Do");
+
+user_pref("mailnews.tags.$label5.color", "#C678DD");
+user_pref("mailnews.tags.$label5.tag", "Later");
+
+// week starts on monday
+user_pref("calendar.week.start", 1);
